6e1543712e8d19f80055f19734b612ec96edc12c
Sirat-chan <sirat.benjemaa@edu.isetcom.tn>
2023-01-24 21:10:13 +0100

    ebanking-initialisation frontend

---
+++ src/app/customers/customers.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {CustomerService} from "../services/customer.service";
+import {catchError, Observable, throwError} from "rxjs";
+import {Customer} from "../model/customer.model";
+import {FormBuilder, FormGroup} from "@angular/forms";
+
+@Component({
+  selector: 'app-customers',
+  templateUrl: './customers.component.html',
+  styleUrls: ['./customers.component.css']
+})
+export class CustomersComponent implements OnInit {
+  customers! : Observable<Array<Customer>>
+  errorMessage! : object;
+  searchFormGroup! : FormGroup;
+  constructor(private customerService : CustomerService, private fb: FormBuilder) { }
+
+  ngOnInit(): void {
+    this.searchFormGroup=this.fb.group({
+      keyword : this.fb.control("")
+    });
+
+    this.customers = this.customerService.getCustomers().pipe(
+      catchError(err => {
+        this.errorMessage=err.message;
+        return throwError(err);
+      })
+    )
+
+  }
+
+  handleSearchCustomers() {
+
+  }
+}
---
+++ src/app/services/customer.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
+import {Customer} from "../model/customer.model";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CustomerService {
+
+  constructor(private http:HttpClient) { }
+
+  public getCustomers():Observable<Array<Customer>>{
+    return this.http.get<Array<Customer>>("http://localhost:8085/customers")
+  }
+}
